a795154d8158d193b03a5d9e9c47d8c44c29660e
Nada Hasni <nadasthing@gmail.com>
2024-05-10 10:05:11 +0700

    feat: init nextjs and shadcn-ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Swell Match",
+  description: "Book your surfing experience",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
